7f189ed796df89d8c2113c43ae6d6bbaca811696
Ashif <ashif.dev0@gmail.com>
2026-08-11 12:26:53 +0530

    Update servo connection details in Connection_Summery.ino
    Added detailed servo connection information.

--- Connection_Summery.ino
+++ Connection_Summery.ino
@@ -6,3 +6,8 @@ Arduino D2  → YL servo signal
 External 5V + → All servo VCC wires
 External GND  → All servo GND wires
 Arduino GND   → External supply GND
+
+Servo Connection
+Red    → +5V
+Brown/Black → GND
+Orange/Yellow/White → Signal
